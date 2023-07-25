a0e660459a5161d0c2b478b292ae1685066c9c86
Nikhil Garg <gargnandan01651@gmail.com>
2023-07-25 21:19:12 +0530

    second commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const URI = process.env.MONGODB_URL || "mongodb://localhost:27017/EmployeeDB";
 
 const abc =
+  process.env.MONGODB_URL ||
   "mongodb+srv://csecec1803181:nikhil01651@cluster0.qwsgl1o.mongodb.net/EmployeeDB";
 mongoose
   .connect(abc, {
